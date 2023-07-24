d230076633ca5d5d8e1f971b5b1aefa39128ef4c
ctanush <ctanush56@gmail.com>
2023-07-24 18:31:45 +0530

    1

--- server.js
+++ server.js
@@ -10,16 +10,25 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    
+    return res.send('Hello World!'),200;
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const rq = req.body;
+    const frst = rq['first'];
+    const sec = rq['second'];
+    let ans = frst+sec;
+    res.json({ "result": ans });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const rq = req.body;
+    const frst = rq['first'];
+    const sec = rq['second'];
+    let ans = frst-sec;
+    res.json({ "result": ans });
 });
 
 app.use(baseUrl, baseRouter);
